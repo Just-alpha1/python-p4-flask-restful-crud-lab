780f632c3156482a1666c78a3c4f5f66b706a9d6
Collins Kiaritha <thuocollins020@gmail.com>
2025-12-24 13:45:19 +0300

    added class plant in app.py and database query

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 
+import pytest
+from app import app, db
+from models import Plant
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_database():
+    with app.app_context():
+        db.create_all()
+        # seed the database
+        Plant.query.delete()
+        aloe = Plant(
+            id=1,
+            name="Aloe",
+            image="./images/aloe.jpg",
+            price=11.50,
+            is_in_stock=True,
+        )
+        zz_plant = Plant(
+            id=2,
+            name="ZZ Plant",
+            image="./images/zz-plant.jpg",
+            price=25.98,
+            is_in_stock=False,
+        )
+        db.session.add_all([aloe, zz_plant])
+        db.session.commit()
+
 def pytest_itemcollected(item):
     par = item.parent.obj
     node = item.obj
